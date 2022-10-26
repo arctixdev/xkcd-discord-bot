97d5ac336f7804c148e333f2276e860473fd7b3c
Jonathan Bangert <jonathan@bangert.dk>
2022-10-26 10:16:03 +0200

    It now works

--- discord_bot.py
+++ discord_bot.py
@@ -1,3 +1,4 @@
+from http.client import ResponseNotReady
 from multiprocessing.util import get_temp_dir
 from optparse import Option
 from ssl import Options
@@ -16,18 +17,24 @@ number_option = discord.Option(
     "The specific comic to send",
     name="number",
     type=int,
+    default=False,
     input_type=int,
     required=False,
     min_value=1,
-    max_value=3000
+    max_value=3000,
 )
 
-command_options=[number_option]
+command_options = [number_option]
+
 
 def random_comic_number():
-    max = json.loads(requests.get("https://xkcd.com/info.0.json").text)["num"]
+    max = current_comic_number()
     number = random.randint(1, max)
     return number
+
+
+def current_comic_number():
+    return int(json.loads(requests.get("https://xkcd.com/info.0.json").text)["num"])
 
 
 def get_info(number):
@@ -45,34 +52,43 @@ def get_embed(number):
         color=discord.Colour.dark_purple(),
     )
     embed.set_image(url=img_url)
-    #embed.add_field(False,"h","h")
+    # embed.add_field(False,"h","h")
     return embed
 
 
-@bot.slash_command(name="random", description="Return a random XKCD comic")
-async def random_comic(ctx):
-    number = random_comic_number()
-    embed = get_embed(number)
-    await ctx.respond(embed=embed)
-
-
-@bot.slash_command(name="comic", description="Return a XKCD comic", Options=command_options)
+@bot.slash_command(
+    name="comic", description="Return a XKCD comic", Options=command_options
+)
 async def get_comic(ctx, numberopt=number_option):
-    if (numberopt):
-        number=numberopt
-    else:
-        number = random_comic_number()
+    number = random_comic_number()
+    if numberopt:
+        try:
+            number = int(numberopt)  # type: ignore
+            if number > 0 and number < current_comic_number():
+                pass
+            else:
+                await ctx.respond(
+                    f"Comic number must be empty for random or between 1 and {current_comic_number()}"
+                )
+                return
+        except:
+            await ctx.respond("ERROR... Option must be empty or a number")
+            return
     embed = get_embed(number)
     await ctx.respond(embed=embed)
+
 
 @bot.slash_command(name="info", description="Show info")
 async def info(ctx):
-    await ctx.respond("""
+    await ctx.respond(
+        """
     This bot is made by @Un1ocked_#0284
     It show comics from https://xkcd.com/ made by Randall Munroe
     Usage:
         /comic      For a random comic
         /comic x   For comic number x
-    """)
+    """
+    )
+
 
 bot.run(TOKEN)
