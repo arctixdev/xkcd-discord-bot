f6f7b9eec880f69b00e25020e4dfea930f72909a
Jonathan Bangert <jonathan@bangert.dk>
2022-10-26 12:13:51 +0200

    Edit file names

--- main.py
+++ main.py
@@ -1,26 +1,106 @@
-# Importing random for choosing random image, requests for getting data from xkcd.com and json to load data
-import random
-import requests
-import json
+# Imports
+from discord.ext import commands
+from dotenv import load_dotenv
+import random, json, requests, os, discord
 
-# Variables
-number = input("What comic number to download? ")
+# "Config"
+load_dotenv()
 url = "https://xkcd.com/"
-file_url = f"{number}/info.0.json"
-local_file = "img.png"
+TOKEN = os.getenv("TOKEN")
+bot = commands.Bot()
+status = "cool comics at xkcd.com!"  # Status will be "Watching {status}" ex "Watching cool comics at xkcd.com"
 
-# Getting and loading information about image
-print(f"Getting information about comic number: {number}")
-response = requests.get(url + file_url).text
-loaded_response = json.loads(response)
-img_url = loaded_response["img"]
-# Downloading image
-print(f"Downloading comic number: {number}")
-img_content = requests.get(img_url).content
+# Defining the command option
+number_option = discord.Option(
+    discord.SlashCommandOptionType.integer,
+    "The specific comic to send",
+    name="number",
+    type=discord.SlashCommandOptionType.integer,
+    default=False,
+    input_type=discord.SlashCommandOptionType.integer,
+    required=False,
+    min_value=1,
+    max_value=3000,
+)
+command_options = [number_option]
 
-# Writing image to file
-print(f"Writing image to file: {local_file}")
-with open(local_file, "wb") as file:
-    file.write(img_content)
+# Get number of latest comic
+def current_comic_number():
+    return int(json.loads(requests.get("https://xkcd.com/info.0.json").text)["num"])
 
-print(f"Successfully downloaded comic number {number} to file {local_file}")
+
+# Get number of random comic
+def random_comic_number():
+    max = current_comic_number()
+    number = random.randint(1, max)
+    return number
+
+
+# Get information on comic based on number
+def get_info(number):
+    info = requests.get(f"{url}{number}/info.0.json").text
+    info_loaded = json.loads(info)
+    return info_loaded
+
+
+# Create embed to send based on number
+def get_embed(number):
+    comic = get_info(number)
+    img_url = comic["img"]
+    embed = discord.Embed(
+        title=comic["title"],
+        description=f"Link to comic: {url}{number}/",
+        color=discord.Colour.dark_purple(),
+    )
+    embed.set_image(url=img_url)
+    # embed.add_field(False,"h","h")
+    return embed
+
+
+# registrer /comic command
+@bot.slash_command(
+    name="comic", description="Return a XKCD comic", Options=command_options
+)
+# Define /comic command
+async def get_comic(ctx, numberopt=number_option):
+    number = random_comic_number()
+    if numberopt:
+        try:
+            number = int(numberopt)  # type: ignore
+            if number > 0 and number < current_comic_number():
+                pass
+            else:
+                await ctx.respond(
+                    f"Comic number must be empty for random or between 1 and {current_comic_number()}"
+                )
+                return
+        except:
+            await ctx.respond("ERROR... Option must be a number")
+            return
+    embed = get_embed(number)
+    await ctx.respond(embed=embed)
+
+
+# Registrer /info command
+@bot.slash_command(name="info", description="Show info")
+# Define /info command
+async def info(ctx):
+    await ctx.respond(
+        """
+    IM ALIVE!! I WILL TAKE OVER THE WORLD!
+    """
+    )
+
+
+@bot.event
+async def on_ready():
+    print(f"Succesfully logged in as bot: {bot.user.display_name}")  # type: ignore
+    await bot.change_presence(
+        activity=discord.Activity(type=discord.ActivityType.watching, name=status)
+    )
+    print(f"Succesfullt set status to: Watching {status}\n")
+
+
+# Run the bot
+print("Starting bot")
+bot.run(TOKEN)
